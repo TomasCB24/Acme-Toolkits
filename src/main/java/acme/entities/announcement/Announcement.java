63b80d8d2bdea222ebf58075565e6847320bbb69
Tomas Camero <tcamerob@gmail.com>
2022-03-05 13:29:06 +0100

    Task 039: Done

---
+++ src/main/java/acme/entities/announcement/Announcement.java
@@ -0,0 +1,47 @@
+package acme.entities.announcement;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Announcement extends AbstractEntity{
+
+	// Serialisation identifier -----------------------------------------------
+
+		protected static final long	serialVersionUID	= 1L;
+		
+	// Attributes -------------------------------------------------------------
+
+		@Past
+		protected LocalDate creationMoment;
+		
+		@NotBlank
+		@Length(max = 101)
+		protected String title;
+		
+		@NotBlank
+		@Length(max = 256)
+		protected String body;
+		
+		@NotNull
+		protected Boolean flag;
+		
+		@URL
+		protected String link;
+		
+		
+
+}
