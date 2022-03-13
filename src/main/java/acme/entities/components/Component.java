19e27bbbc28758b5c7c95d33992941836c9f433e
Ismael Pérez Ortiz <ismaelperezortiz@gmail.com>
2022-03-13 16:19:11 +0100

    Task 040: fixed

--- src/main/java/acme/entities/components/Component.java
+++ src/main/java/acme/entities/components/Component.java
@@ -1,11 +1,17 @@
 package acme.entities.components;
 
+import java.awt.Toolkit;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
@@ -40,14 +46,21 @@ public class Component extends AbstractEntity {
 	@Length(max = 256)
 	protected String			description;
 	
-	
+	@NotNull
+	@Valid
 	protected Money				retailPrice;
-
+	
+	@URL
 	protected String 			link;
 	
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+    @Valid
+    @ManyToOne(optional = false)
+    protected Toolkit toolkit;
 
 }
