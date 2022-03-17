4ce9f2c6a5a3058909eddf13f1d08317ac039b4a
juradosalado <juansaladous@gmail.com>
2022-03-17 13:07:58 +0100

    Task 044: Fixed

--- src/main/java/acme/entities/patronage/PatronageReport.java
+++ src/main/java/acme/entities/patronage/PatronageReport.java
@@ -2,6 +2,7 @@ package acme.entities.patronage;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -29,7 +30,8 @@ public class PatronageReport extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 	
-	@NotBlank
+	
+	@Column(unique=true)
 	@Pattern(regexp = "^[0-9]{4}$")
 	protected String serialNumber;
 	
@@ -40,7 +42,7 @@ public class PatronageReport extends AbstractEntity {
 	protected Date	creationMoment;
 	
 	@NotBlank
-	@Length(max=256)
+	@Length(min=1, max=255)
 	protected String memorandum;
 	
 	@URL
@@ -56,7 +58,7 @@ public class PatronageReport extends AbstractEntity {
 
 	@NotNull
 	@Valid
-	@ManyToOne(optional = false)
+	@ManyToOne(optional = true)
 	protected Patronage patronage;
 	
 }
