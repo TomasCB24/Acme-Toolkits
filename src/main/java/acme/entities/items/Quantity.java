f8ac0199b0b7eaada53a4cd6bd8542674e9b254e
juradosalado <juansaladous@gmail.com>
2022-03-17 21:25:19 +0100

    Preparing for Follow Up

--- src/main/java/acme/entities/items/Quantity.java
+++ src/main/java/acme/entities/items/Quantity.java
@@ -1,5 +1,6 @@
 package acme.entities.items;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -7,7 +8,12 @@ import javax.validation.constraints.NotNull;
 
 import acme.entities.toolkits.Toolkit;
 import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Quantity extends AbstractEntity{
 	
 	// Serialisation identifier -----------------------------------------------
