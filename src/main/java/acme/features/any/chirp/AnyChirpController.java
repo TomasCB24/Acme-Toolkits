e2a474a7f71916aae7b244b4c9e2561cd7a6d4b9
pabrivjim <parijimenez@gmail.com>
2022-04-04 18:46:04 +0200

    Task 002: done

---
+++ src/main/java/acme/features/any/chirp/AnyChirpController.java
@@ -0,0 +1,27 @@
+package acme.features.any.chirp;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.chirps.Chirp;
+import acme.framework.controllers.AbstractController;
+import acme.framework.roles.Any;
+
+@Controller
+public class AnyChirpController extends AbstractController<Any, Chirp>{
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AnyChirpListService				listService;
+	
+
+	// Constructors -----------------------------------------------------------
+	
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list", "list", this.listService);
+	}	
+}
