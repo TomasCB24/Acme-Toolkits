20731f3177de10f90a5fa2e1e02131f90eea2a45
pabrivjim <parijimenez@gmail.com>
2022-04-05 18:36:44 +0200

    Task 013: in progress

---
+++ src/main/java/acme/features/patron/patronage/PatronPatronageController.java
@@ -0,0 +1,29 @@
+package acme.features.patron.patronage;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.patronages.Patronage;
+import acme.framework.controllers.AbstractController;
+import acme.roles.Patron;
+
+@Controller
+public class PatronPatronageController extends AbstractController<Patron, Patronage> {
+
+	@Autowired
+	protected PatronPatronageListMineService		listService;
+
+	@Autowired
+	protected PatronPatronageShowService			showService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("show", this.showService);
+		super.addCommand("list", this.listService);
+	}
+}
--- src/main/webapp/WEB-INF/views/fragments/menu.jsp
+++ src/main/webapp/WEB-INF/views/fragments/menu.jsp
@@ -47,6 +47,10 @@
 			<acme:menu-suboption code="master.menu.consumer.favourite-link" action="http://www.example.com/"/>
 		</acme:menu-option>
 	</acme:menu-left>
+	
+	<acme:menu-option code="master.menu.patron" access="hasRole('Patron')">
+			<acme:menu-suboption code="master.menu.patron.patronage.list" action="/patron/patronage/list"/>
+	</acme:menu-option>
 
 	<acme:menu-right>
 		<acme:menu-option code="master.menu.sign-up" action="/anonymous/user-account/create" access="isAnonymous()"/>
