96d90673bb8142f705ae8a54f2147d25ef3187d7
Tomas Camero <tcamerob@gmail.com>
2022-04-18 21:19:34 +0200

    Task 006: Remove request mapping on announcement controller

--- src/main/java/acme/features/authenticated/announcement/AuthenticatedAnnouncementController.java
+++ src/main/java/acme/features/authenticated/announcement/AuthenticatedAnnouncementController.java
@@ -16,14 +16,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import acme.entities.announcements.Announcement;
 import acme.framework.controllers.AbstractController;
 import acme.framework.roles.Authenticated;
 
 @Controller
-@RequestMapping("/authenticated/announcement/")
 public class AuthenticatedAnnouncementController extends AbstractController<Authenticated, Announcement> {
 
 	// Internal state ---------------------------------------------------------
