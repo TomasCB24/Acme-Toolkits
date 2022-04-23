591abed98723c4aacd17b935290eaaffeb5efb2f
Ismael Pérez Ortiz <ismaelperezortiz@gmail.com>
2022-04-23 16:29:40 +0200

    Task 010/T1: done

--- src/test/java/acme/testing/inventor/toolkit/InventorToolkitShowMineTest.java
+++ src/test/java/acme/testing/inventor/toolkit/InventorToolkitShowMineTest.java
@@ -1,6 +1,116 @@
 package acme.testing.inventor.toolkit;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class InventorToolkitShowMineTest {
+import acme.testing.TestHarness;
+
+public class InventorToolkitShowMineTest extends TestHarness{
+
+	// Test cases -------------------------------------------------------------
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/toolkit/show-mine.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestShowToolkits(final int recordIndex, final String code, final String title, final String description, final String assemblyNotes, final String link, final String retailPrice) {
+		super.signIn("inventor1", "inventor1");
+
+		super.clickOnMenu("Inventor", "List my Toolkits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, title);
+		
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("assemblyNotes", assemblyNotes);
+		
+		
+		if(!link.equals("null")) {
+			super.checkInputBoxHasValue("link", link);
+		} else {
+			super.checkInputBoxHasValue("link", "");
+		}
+		
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+
+		
+		
+		super.checkButtonExists("Items");
+		super.clickOnButton("Items");
+		
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+	
+		
+	}
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/toolkit/list-items-first-toolkit.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void positiveTestListingItemsOfToolkits(final int recordIndex, final String type, final String name, final String retailPrice, final String quantity) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "List my Toolkits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(0);	// first toolkit
+		super.checkButtonExists("Items");
+		super.clickOnButton("Items");
+		
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, type);
+		super.checkColumnHasValue(recordIndex, 1, name);
+		super.checkColumnHasValue(recordIndex, 2, retailPrice);
+		super.checkColumnHasValue(recordIndex, 3, quantity);
+		
+	}
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/toolkit/show-items-first-toolkit.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(30)
+	public void positiveTestShowingItemsOfToolkits(final int recordIndex, final String type, final String name, final String retailPrice, final String code, final String technology, final String description, final String link) {
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "List my Toolkits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(0);	// first toolkit
+		super.checkButtonExists("Items");
+		super.clickOnButton("Items");
+		
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		
+		super.checkInputBoxHasValue("type", type);
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		
+		if(!link.equals("null")) {
+			super.checkInputBoxHasValue("link", link);
+		} else {
+			super.checkInputBoxHasValue("link", "");
+		}
+		
+		
+	}
+
+	
+	
+	
 
 }
