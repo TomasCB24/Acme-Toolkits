4061faac8c3762dbc0e263f12e28d15243dab260
Ismael Pérez Ortiz <ismaelperezortiz@gmail.com>
2022-04-23 11:54:52 +0200

    Task 007/T1: done

---
+++ src/test/java/acme/testing/authenticated/systemconfiguration/AuthenticatedSystemConfigurationShowCurrencyTest.java
@@ -0,0 +1,42 @@
+package acme.testing.authenticated.systemconfiguration;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AuthenticatedSystemConfigurationShowCurrencyTest extends TestHarness {
+	
+	
+	// Test cases -------------------------------------------------------------
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/systemconfiguration/show-currency.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestAuthenticated(final String recordIndex, final String acceptedCurrencies, final String systemCurrency) {
+		super.signIn("patron1", "patron1");
+		super.clickOnMenu("Authenticated", "Currency System Configuration");
+		super.checkFormExists();
+		
+		super.checkInputBoxHasValue("acceptedCurrencies", acceptedCurrencies);
+		super.checkInputBoxHasValue("systemCurrency", systemCurrency);
+		
+		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/systemconfiguration/show-currency.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void NegativeTestAnonymous(final String recordIndex, final String acceptedCurrencies, final String systemCurrency) {
+		
+		super.navigate("/authenticated/system-configuration/show");
+		super.checkPanicExists();
+		
+	}
+	
+
+	
+	
+	
+	
+}
