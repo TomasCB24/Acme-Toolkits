6594c031949cb44c930806d82a802f02785d4814
Miguel <miguelromeroarjona@gmail.com>
2022-04-24 14:05:55 +0200

    Task 003-T1: done

---
+++ src/test/java/acme/testing/any/item/AnyItemListPublishedTest.java
@@ -0,0 +1,29 @@
+package acme.testing.any.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyItemListPublishedTest extends TestHarness{
+	
+	// Test cases -------------------------------------------------------------
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/item/list-items.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestListingItemsOfToolkits(final int recordIndex, final String type, final String name, final String retailPrice, final String quantity) {
+		
+		super.clickOnMenu("Anonymous", "Published Items list");
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, type);
+		super.checkColumnHasValue(recordIndex, 1, name);
+		super.checkColumnHasValue(recordIndex, 2, retailPrice);
+		super.checkColumnHasValue(recordIndex, 3, quantity);
+		
+	}
+	
+}
---
+++ src/test/java/acme/testing/any/item/AnyItemShowTest.java
@@ -0,0 +1,40 @@
+package acme.testing.any.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyItemShowTest extends TestHarness{
+
+	// Test cases -------------------------------------------------------------	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/item/show-items.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestShowingItemsOfToolkits(final int recordIndex, final String type, final String name, final String retailPrice, final String code, final String technology, final String description, final String link, final String inventor) {
+		
+		super.clickOnMenu("Anonymous", "Published Items list");
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("inventor", inventor);
+		
+		if(!link.equals("null")) {
+			super.checkInputBoxHasValue("link", link);
+		} else {
+			super.checkInputBoxHasValue("link", "");
+		}
+			
+	}
+	
+}
