9827942db96cb707d422c9949dd0373bdedbd94d
Tomas Camero <tcamerob@gmail.com>
2022-05-03 12:08:52 +0200

    Task 009: Done

---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementCreateService.java
@@ -0,0 +1,106 @@
+/*
+ * AdministratorAnnouncementCreateService.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator.announcement;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.announcements.Announcement;
+import acme.features.authenticated.announcement.AuthenticatedAnnouncementRepository;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Errors;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Administrator;
+import acme.framework.services.AbstractCreateService;
+
+
+@Service
+public class AdministratorAnnouncementCreateService implements AbstractCreateService<Administrator, Announcement> {
+
+	@Autowired
+	protected AuthenticatedAnnouncementRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Announcement> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Announcement> request, final Announcement entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors, "title", "body", "flag", "link");
+	}
+
+	@Override
+	public void unbind(final Request<Announcement> request, final Announcement entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "title", "body", "flag", "link");
+		model.setAttribute("confirmation", false);
+		model.setAttribute("readonly", false);
+	}
+
+	@Override
+	public Announcement instantiate(final Request<Announcement> request) {
+		assert request != null;
+
+		Announcement result;
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+
+		result = new Announcement();
+		result.setTitle("");
+		result.setCreationMoment(moment);
+		result.setBody("");
+		result.setFlag(false);
+		result.setLink("");
+
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Announcement> request, final Announcement entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		boolean confirmation;
+
+		confirmation = request.getModel().getBoolean("confirmation");
+		errors.state(request, confirmation, "confirmation", "javax.validation.constraints.AssertTrue.message");
+	}
+
+	@Override
+	public void create(final Request<Announcement> request, final Announcement entity) {
+		assert request != null;
+		assert entity != null;
+
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		entity.setCreationMoment(moment);
+		this.repository.save(entity);
+	}
+
+}
---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementShowService.java
@@ -0,0 +1,61 @@
+/*
+ * AuthenticatedAnnouncementShowService.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator.announcement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.announcements.Announcement;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Administrator;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AdministratorAnnouncementShowService implements AbstractShowService<Administrator, Announcement> {
+
+	@Autowired
+	protected AdministratorAnnouncementRepository repository;
+
+	@Override
+	public boolean authorise(final Request<Announcement> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public Announcement findOne(final Request<Announcement> request) {
+		assert request != null;
+
+		Announcement result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneAnnouncementById(id);
+
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Announcement> request, final Announcement entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "creationMoment", "title", "body", "link");
+		model.setAttribute("confirmation", false);
+		model.setAttribute("readonly", true);
+	}
+
+}
