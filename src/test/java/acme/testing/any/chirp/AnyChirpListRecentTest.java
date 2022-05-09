adad575b6050d10e0fef211dd4ad63779c6c344e
Miguel <miguelromeroarjona@gmail.com>
2022-05-09 15:56:30 +0200

    Task 021: done

--- src/test/java/acme/testing/any/chirp/AnyChirpListRecentTest.java
+++ src/test/java/acme/testing/any/chirp/AnyChirpListRecentTest.java
@@ -5,20 +5,25 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import acme.testing.TestHarness;
+import acme.testing.TemporalAwareTestHarness;
 
-public class AnyChirpListRecentTest  extends TestHarness {
+public class AnyChirpListRecentTest  extends TemporalAwareTestHarness {
 	
 	// Test cases -------------------------------------------------------------
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/list-recent.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String creationMoment, final String title, final String author, final String body, final String email) {
+	public void positiveTest(final int recordIndex, final int deltaDays, final String title, final String author, final String body, final String email) {
+		
+		String moment;
+
+        moment = super.computeDeltaMoment(deltaDays);
+		
 		super.clickOnMenu("Anonymous", "List recents chirps");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, author);
 		super.checkColumnHasValue(recordIndex, 3, body);
--- src/test/java/acme/testing/authenticated/announcement/AuthenticatedAnnouncementListTest.java
+++ src/test/java/acme/testing/authenticated/announcement/AuthenticatedAnnouncementListTest.java
@@ -5,22 +5,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import acme.testing.TestHarness;
+import acme.testing.TemporalAwareTestHarness;
 
-public class AuthenticatedAnnouncementListTest extends TestHarness  {
+public class AuthenticatedAnnouncementListTest extends TemporalAwareTestHarness  {
 
 	// Test cases -------------------------------------------------------------
 	@ParameterizedTest
 	@CsvFileSource(resources = "/authenticated/announcement/list-recent.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String creationMoment, final String title) {
+	public void positiveTest(final int recordIndex, final int deltaDays, final String title) {
+		
+		String moment;
+
+        moment = super.computeDeltaMoment(deltaDays);
 		
 		super.signIn("inventor1", "inventor1");
 		super.clickOnMenu("Authenticated", "List recent announcements");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.signOut();
 		
