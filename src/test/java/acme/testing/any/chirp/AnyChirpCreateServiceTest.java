be1fcd69050abea40069ec14487ea1649cce46df
Ismael Pérez Ortiz <ismaelperezortiz@gmail.com>
2022-05-23 16:31:46 +0200

    Task 002/T1: done

---
+++ src/test/java/acme/testing/any/chirp/AnyChirpCreateServiceTest.java
@@ -0,0 +1,67 @@
+package acme.testing.any.chirp;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyChirpCreateServiceTest extends TestHarness{
+	/*
+	 * Casos positivos: campos comprobando mínimo y máximo
+	 * Casos negativos: campos que superen el máximo
+	 */
+	
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/chirp/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10) 
+	public void positiveTest(final int recordIndex, final String title, final String body, final String author, final String mail)  {
+		super.clickOnMenu("Anonymous", "List recents chirps");
+		super.checkListingExists();
+		
+		super.clickOnButton("Create");
+		super.checkFormExists();
+		
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("email", mail);
+		super.fillInputBoxIn("confirmation", "true");
+		super.clickOnSubmit("Create");
+		
+		super.checkListingExists();
+		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 2, body);
+		super.checkColumnHasValue(recordIndex, 3, author);
+		super.checkColumnHasValue(recordIndex, 4, mail);
+	
+	}
+
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/chirp/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20) 
+	public void negativeTest(final int recordIndex, final String title, final String body, final String author, final String mail, final String confirmation)  {
+		super.clickOnMenu("Anonymous", "List recents chirps");
+		super.checkListingExists();
+		
+		super.clickOnButton("Create");
+		super.checkFormExists();
+		
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("email", mail);
+		super.fillInputBoxIn("confirmation", confirmation);
+		super.clickOnSubmit("Create");
+		
+		super.checkErrorsExist();
+		
+	
+	}
+	
+
+	
+
+}
